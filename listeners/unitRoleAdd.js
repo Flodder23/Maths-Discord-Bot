dab21bffefbde3038266fb872bff60f61ece6da2
Joe Gibson <joe.gibby@hotmail.co.uk>
2020-03-18 21:46:02 +0000

    bot now ignores itself and other bots

--- listeners/unitRoleAdd.js
+++ listeners/unitRoleAdd.js
@@ -14,29 +14,27 @@ class UnitRoleListener extends Listener {
 	}
 
 	async exec(message) {
-		if (this.client.testMode != (guild[1].name != "Lonely Joe")) {
-			for (let guild of this.client.guilds) {
-				if (message.channel.name=="unit-roles") {
-					let found_unit = false;
-					for (let line of message.content.split("\n")) {
-						for (let word of line.split(" ")) {
-							word = word.toUpperCase()
-							if (isUnit.test(word)) {
-								for (let role of message.guild.roles) {
-									if (role[1].name == word) {
-										await message.member.addRole(role[1])
-										found_unit = true;
-										break
-									}
+		if (this.client.testMode != (message.guild.name != "Lonely Joe") && !message.author.bot) {
+			if (message.channel.name == "unit-roles") {
+				let found_unit = false;
+				for (let line of message.content.split("\n")) {
+					for (let word of line.split(" ")) {
+						word = word.toUpperCase()
+						if (isUnit.test(word)) {
+							for (let role of message.guild.roles) {
+								if (role[1].name == word) {
+									await message.member.addRole(role[1])
+									found_unit = true;
+									break
 								}
 							}
 						}
 					}
-					if(found_unit) {
-						await message.react(config.ok_hand);
-					} else {
-						await message.react(config.thumbs_down);
-					}
+				}
+				if(found_unit) {
+					await message.react(config.ok_hand);
+				} else {
+					await message.react(config.thumbs_down);
 				}
 			}
 		}
