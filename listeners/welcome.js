dc9edf48370b50c6713224639f3410f4d77a501e
Joe Gibson <joe.gibby@hotmail.co.uk>
2020-05-12 13:43:53 +0100

    Update welcome.js

--- listeners/welcome.js
+++ listeners/welcome.js
@@ -12,8 +12,13 @@ class WelcomeListener extends Listener {
 	}
 
 	exec(member) {
-		if (this.client.testMode != (member.guild.name != "Lonely Joe")) {
-			member.guild.systemChannel.send(`Welcome to the **${member.guild.name}** server, ${member}`);
+		if (this.client.testMode == (member.guild.name == "Lonely Joe")) {
+			if (member.guild.systemChannel.name == "unit-roles") {
+				channel_name = "this channel"
+			} else {
+				channel_name = "<#689460678762954801>"
+			}
+			member.guild.systemChannel.send(`Welcome to the **${member.guild.name}** server, ${member}! Put the names of the units you're taking in ${channel_name} to be assigned the appropriate roles.`);
 		}
 	}
 }
